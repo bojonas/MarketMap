edf427de36ef979bcbad1d5dbdfbacb3bbbd39e7
Jonas <115617616+bojonas@users.noreply.github.com>
2024-04-26 09:30:18 +0200

    für dich Ben

--- src/routes/Login/Login.jsx
+++ src/routes/Login/Login.jsx
@@ -1,7 +1,11 @@
 export default function Login({width, height}){
     return (
-        <div>
-            Hallo
+        <div className='flex bg-slate-600 h-full'>
+            <div className='bg-slate-700 w-full'>
+                <div className='bg-slate-700 h-[90vh] max-h-[90vh] content-center justify-center text-center'>
+                    <p>Login</p>
+                </div>
+            </div>
         </div>
     )
 }
