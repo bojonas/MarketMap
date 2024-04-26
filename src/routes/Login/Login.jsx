68706a5e6c3da8d97910d668a6ae14a6ce13d53e
benroehrig <ben_roehrig@icloud.com>
2024-04-26 14:13:55 +0200

    index.css und Login.jsx geändert

--- src/routes/Login/Login.jsx
+++ src/routes/Login/Login.jsx
@@ -1,11 +1,18 @@
-export default function Login({width, height}){
-    return (
-        <div className='flex bg-slate-600 h-full'>
-            <div className='bg-slate-700 w-full'>
-                <div className='bg-slate-700 h-[90vh] max-h-[90vh] content-center justify-center text-center'>
-                    <p>Login</p>
-                </div>
-            </div>
+export default function Login({ width, height }) {
+  return (
+    <div className='bg-slate-700 flex items-center justify-center h-full'>
+      <div className='bg-slate-700 rounded-lg p-8'>
+        <div className='bg-slate-700 flex flex-col items-center justify-center'>
+          <input id="login_username" type="text" placeholder="Username" className="custom-input mb-4" />
+          <input type="password" placeholder="Password" className="custom-input mb-8" />
+          <button className="custom-button" onClick={login_button()}>Login</button>
+          <p className="text-red-700" hidden>invalid password</p>
         </div>
-    )
+      </div>
+    </div>
+  );
+}
+
+function login_button(){
+console.log("Hallo")
 }
