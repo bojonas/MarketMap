e1d2ad3d6f3d82811c91ca22e0005c296510af74
benroehrig <ben_roehrig@icloud.com>
2024-04-29 11:05:22 +0200

    index und Login geändert

--- src/routes/Login/Login.jsx
+++ src/routes/Login/Login.jsx
@@ -1,12 +1,13 @@
-export default function Login({ width, height }) {
+export default function Login() {
   return (
     <div className='bg-slate-700 flex items-center justify-center h-full'>
       <div className='bg-slate-700 rounded-lg p-8'>
         <div className='bg-slate-700 flex flex-col items-center justify-center'>
           <input id="login_username" type="text" placeholder="Username" className="custom-input mb-4" />
-          <input type="password" placeholder="Password" className="custom-input mb-8" />
-          <button className="custom-button" onClick={login_button()}>Login</button>
-          <p className="text-red-700" hidden>invalid password</p>
+          <input id="login_password" type="password" placeholder="Password" className="custom-input mb-8" />
+          <button className="custom-button" onClick={login_button}>Login</button>
+          <p id="reset_pw" onclick={forgot_password}>Forgot Password?</p>
+          <p id = "error_box" className="text-red-700 invisible"></p>
         </div>
       </div>
     </div>
@@ -14,5 +15,22 @@ export default function Login({ width, height }) {
 }
 
 function login_button(){
-console.log("Hallo")
+  //todo: DB logik
+  var pw = "1234"
+
+  const username = document.getElementById("login_username").value
+  const password = document.getElementById("login_password").value
+
+  if(pw === password){
+    //Log User in
+  }
+  else{
+    document.getElementById("error_box").style.visibility = 'visible'
+    document.getElementById("error_box").innerText = username+" "+password
+  }
+}
+
+function forgot_password(){
+
+  
 }
