8459eaaea05de0b9fa23ccedcccbcd5d36e61544
benroehrig <ben_roehrig@icloud.com>
2024-04-30 15:19:52 +0200

    Changes

--- src/routes/Login/Login.jsx
+++ src/routes/Login/Login.jsx
@@ -1,36 +1,50 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const login = () => {
+    if (password === "1234") {
+      // Führe den Login durch
+      setErrorMessage("");
+    } else {
+      setErrorMessage("Invalid Credentials");
+    }
+  };
+
+  const forgotPassword = () => {
+    console.log("Hallo");
+  };
+
   return (
-    <div className='bg-slate-700 flex items-center justify-center h-full'>
-      <div className='bg-slate-700 rounded-lg p-8'>
-        <div className='bg-slate-700 flex flex-col items-center justify-center'>
-          <input id="login_username" type="text" placeholder="Username" className="custom-input mb-4" />
-          <input id="login_password" type="password" placeholder="Password" className="custom-input mb-8" />
-          <button className="custom-button" onClick={login_button}>Login</button>
-          <p id="reset_pw" onclick={forgot_password}>Forgot Password?</p>
-          <p id = "error_box" className="text-red-700 invisible"></p>
+    <div className="bg-slate-700 flex items-center justify-center h-full">
+      <div className="bg-slate-700 rounded-lg p-8">
+        <div className="bg-slate-700 flex flex-col items-center justify-center">
+          <input
+            type="text"
+            placeholder="Username"
+            className="custom-input mb-4"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="custom-input mb-8"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="custom-button" onClick={login}>
+            Login
+          </button>
+          <p id="reset_pw" onClick={forgotPassword}>
+            Forgot Password?
+          </p>
+          <p className="text-red-700">{errorMessage}</p>
         </div>
       </div>
     </div>
   );
-}
-
-function login_button(){
-  //todo: DB logik
-  var pw = "1234"
-
-  const username = document.getElementById("login_username").value
-  const password = document.getElementById("login_password").value
-
-  if(pw === password){
-    //Log User in
-  }
-  else{
-    document.getElementById("error_box").style.visibility = 'visible'
-    document.getElementById("error_box").innerText = username+" "+password
-  }
-}
-
-function forgot_password(){
-
-  
 }
