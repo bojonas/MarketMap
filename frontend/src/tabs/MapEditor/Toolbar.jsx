e079dbeb58b484c620f2a0f6243b7602107457bd
Jonas <115617616+bojonas@users.noreply.github.com>
2024-05-03 10:00:32 +0200

    better logo

--- frontend/src/tabs/MapEditor/Toolbar.jsx
+++ frontend/src/tabs/MapEditor/Toolbar.jsx
@@ -22,8 +22,8 @@ export default function Toolbar({ layout }) {
       <div className='bg-slate-800 flex flex-col items-center text-center h-full rounded-lg m-[5svh]'>
         <div className='flex flex-col content-center items-center text-center rounded-lg m-2 overflow-y-scroll min-h-[58svh] max-h-[58svh] w-[11svw]'>
           {images.map(([type, source], index) => (
-            <div key={index} 
-              className={`bg-slate-700 w-full flex items-center flex-col border-slate-800 rounded-lg p-[1.6svh] ${index === 0 ? 'border-b-2' : index === images.length-1 ? 'border-t-2' : 'border-2'}`}> 
+            <div key={index} className={`bg-slate-700 w-full flex items-center flex-col border-slate-800 rounded-lg p-[1.6svh] 
+              ${index === 0 ? 'border-b-2' : index === images.length-1 ? 'border-t-2' : 'border-2'}`}> 
               <span className='text-white' style={{fontSize: `${scale/4}px`}}>{type.replace('_', ' ')}</span>
               <div className='rounded-md hover:border-[0.2rem] hover:border-purple-custom hover:shadow-purple-custom'
                 style={{ width: `${scale}px`, boxShadow: '0 5px 2px -1px rgb(35 45 65)' }}>
