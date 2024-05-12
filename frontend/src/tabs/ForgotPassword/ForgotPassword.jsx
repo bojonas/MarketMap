c5064373b80117c0f80df83b23ba5f2bcedb4ba6
benroehrig <ben_roehrig@icloud.com>
2024-05-12 14:37:29 +0200

    ForgotPW

---
+++ frontend/src/tabs/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function forgotPassword(){
+    const [email, setUserId] = useState("")
+
+    return (
+        <div className="bg-slate-700 flex items-center justify-center h-full">
+        <div className="bg-slate-700 rounded-lg p-8">
+        <div className="bg-slate-700 flex flex-col items-center justify-center">
+          
+        </div>
+      </div>
+    </div>
+    )
+}
