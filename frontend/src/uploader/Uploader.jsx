2314a04b993eb71037c7b71763fbe5b07c1d97a3
benroehrig <ben_roehrig@icloud.com>
2024-06-06 01:22:53 +0200

    file parser

--- frontend/src/uploader/Uploader.jsx
+++ frontend/src/uploader/Uploader.jsx
@@ -1,7 +1,52 @@
+import { useState, React } from "react"
+import { parseFile } from "./parseFile";
+
 export default function Uploader(){
+    const [content, setContent] = useState()
+    if(content) console.log(content);
+    const handleUpload = ()=>{
+
+
+    }
+
+    const handleFileChange = async (event) => {
+        const file = event.target.files[0];
+        var test;
+        if (file) {
+            if(file.name.endsWith('.txt')){
+                test = await parseFile(file, "txt")
+
+            }
+            else{
+                test = await parseFile(file, "docx")
+            }
+            
+            console.log(test)
+        }
+    };
     return(
-        <div className="flex items-center justify-center text-white">
-            Under Construction!!
+        <div className="w-1/2 h-1/2 m-auto bg-white flex flex-col items-center justify-center text-black">
+            <div className="flex flex-row h-3/4 w-full">
+                <input 
+                    type="text" 
+                    className="w-1/2 h-full bg-red-300 placeholder-black" 
+                    placeholder="Copy Shopping list here"
+                    onChange={(e)=>{setContent(e.target.value)}}
+                />
+                <input 
+                    type="file" 
+                    className="w-1/2 h-full bg-yellow-300 placeholder-black" 
+                    placeholder="Copy Shopping list here"
+                    onChange={handleFileChange}
+                />
+            </div>
+            <div className="h-1/4 w-full flex">
+                <button className="bg-blue-700 w-1/2 h-1/2 m-auto rounded-2xl hover:bg-green-500"
+                    onClick={handleUpload}>
+                    Upload
+                </button>
+            </div>
+            
         </div>
     )
 }
---
+++ frontend/src/uploader/parseFile.js
@@ -0,0 +1,56 @@
+import mammoth from 'mammoth';
+
+export async function parseFile(file, type){
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = async (e) => {
+                var result;
+                if(type === "docx"){
+                    const arrayBuffer = e.target.result;
+                    result = (await mammoth.extractRawText({ arrayBuffer })).value;
+                }
+                else{
+                    result = e.target.result;
+                }
+                const output = parseFileHelper(result)
+                resolve(output); 
+            };
+            reader.onerror = (e) => {
+                reject(e); 
+            };
+            if(type === "docx"){
+                reader.readAsArrayBuffer(file);
+            }
+            else if (type === "txt"){
+                reader.readAsText(file);
+            }
+            else{
+                reject("Invalid File Type")
+            }
+
+        });
+    }
+
+    const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+    function parseFileHelper(text){
+        var rawData = text.split('\n');
+        var output = [];
+        for(let i=0; i<rawData.length; i++){
+            
+            if(rawData[i][0] in numbers){
+                let j = 0
+                while((rawData[i][j] in numbers)){
+                    j+=1;
+                }
+
+                output.push({product: rawData[i].substring(j+1,rawData[i].length).trim(), amount: Number(rawData[i].substring(0,j))});
+            }
+            else if(rawData[i]===""){} //ignore empty lines
+            else{
+                output.push({product: rawData[i].trim(), amount: 1});
+            }
+        }
+
+        return output
+    }
