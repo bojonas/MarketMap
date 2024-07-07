ba6207ada4b00cf4076fe5348b8de42b923efe9c
Jonas <115617616+bojonas@users.noreply.github.com>
2024-07-07 19:36:31 +0200

    s

--- frontend/src/helper/getTimePassed.js
+++ frontend/src/helper/getTimePassed.js
@@ -2,7 +2,8 @@ export function getTimePassed(timestamp) {
     const now = new Date();
     const nowUTC = Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
     const timestampUTC = new Date(timestamp).getTime();
-    const difference = nowUTC - timestampUTC;
+    const offset = now.getTimezoneOffset() * 60000;
+    const difference = nowUTC - timestampUTC - offset;
     let time = Math.round(difference / (1000 * 60)); 
     let entity = 'min';
     if (time >= 60) {
