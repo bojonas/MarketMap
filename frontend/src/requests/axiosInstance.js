c65e6483fd9431f49f45c088d9ff233ba7562059
Jonas <115617616+bojonas@users.noreply.github.com>
2025-01-13 12:15:05 +0100

    update

--- frontend/src/requests/axiosInstance.js
+++ frontend/src/requests/axiosInstance.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 // base url to send requests to
 export const axiosInstance = axios.create({
-  //baseURL: 'http://localhost:3001'
-  baseURL: 'https://marketmap.onrender.com/'
+  baseURL: 'http://localhost:3001'
+  //baseURL: 'https://marketmap.onrender.com/'
 });
